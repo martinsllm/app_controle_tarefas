2e5204ba7b7d39c2d3ce9ad330fdd1c4397e3096
martinsllm <lucaslm2010@gmail.com>
2025-07-04 21:00:18 -0300

    Changing main route

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])
     ->name('home')
     ->middleware('verified');
 
