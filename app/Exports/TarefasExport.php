1d405752ebeceb455524bd940fba507ea6a2b59f
martinsllm <lucaslm2010@gmail.com>
2025-07-07 16:04:33 -0300

    Using map to manipuling rows and format dates

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class TarefasExport implements FromCollection, WithHeadings
+class TarefasExport implements FromCollection, WithHeadings, WithMapping
 {
     /**
      * @return \Illuminate\Support\Collection
@@ -19,11 +20,17 @@ class TarefasExport implements FromCollection, WithHeadings
     {
         return [
             'ID tarefa',
-            'ID Usuário',
             'Tarefa',
-            'Data Limite Conclusão',
-            'Data criação',
-            'Data atualização'
+            'Data Limite Conclusão'
+        ];
+    }
+
+    public function map($row): array
+    {
+        return [
+            $row->id,
+            $row->tarefa,
+            date('d/m/Y', strtotime($row->data_limite_conclusao)),
         ];
     }
 }
