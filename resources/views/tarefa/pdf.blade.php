3ba678d094d07bca16959988f67c5da9fd667b0e
martinsllm <lucaslm2010@gmail.com>
2025-07-07 17:23:57 -0300

    Adding support utf-8, style css and page break in pdf

--- resources/views/tarefa/pdf.blade.php
+++ resources/views/tarefa/pdf.blade.php
@@ -1,20 +1,57 @@
-<h2>Lista de tarefas</h2>
+<html>
 
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Tarefa</th>
-            <th>Data Limite Conclusão</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($tarefas as $key => $tarefa)
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <style>
+        .titulo {
+            border: 1px;
+            background-color: #c2c2c2;
+            text-align: center;
+            width: 100%;
+            text-transform: uppercase;
+            font-weight: bold;
+            margin-bottom: 25px;
+        }
+
+        .tabela {
+            width: 100%
+        }
+
+        .tabela th {
+            text-align: left
+        }
+
+        .page-break {
+            page-break-after: always;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="titulo">Lista de tarefas</div>
+
+    <table class="tabela">
+        <thead>
             <tr>
-                <td>{{ $tarefa->id }}</td>
-                <td>{{ $tarefa->tarefa }}</td>
-                <td>{{ date('d/m/Y', strtotime($tarefa->data_limite_conclusao)) }}</td>
+                <th>ID</th>
+                <th>Tarefa</th>
+                <th>Data Limite Conclusão</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($tarefas as $key => $tarefa)
+                <tr>
+                    <td>{{ $tarefa->id }}</td>
+                    <td>{{ $tarefa->tarefa }}</td>
+                    <td>{{ date('d/m/Y', strtotime($tarefa->data_limite_conclusao)) }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <div class="page-break"></div>
+    <h2>Página 2</h2>
+</body>
+
+</html>
